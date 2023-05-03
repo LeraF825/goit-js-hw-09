5d9653802420ffafb4216fc9d3e27157d5c46e51
LeraF825 <fertikovalera@gmail.com>
2023-05-03 18:16:31 +0300

    task-2 css styles

---
+++ src/js/02-timer.js
@@ -0,0 +1,10 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const inputDate = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
